e9526d2d46fc5c95a1e4fb497d952f5181f9f74b
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-06-19 11:05:12 +0530

    Added motion lib

---
+++ my-first-site/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+function Header() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <motion.h1
+        className='font-bold underline font-playwrite text-6xl text-blue-900 hover:text-green-500 transition-all duration-500'
+        whileHover={{ scale: 1.1 }} // Slightly increase size on hover
+        transition={{ type: 'tween', stiffness: 300 }} // Define the type of transition
+      >
+        Shreyash Shubh
+      </motion.h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ my-first-site/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure this path matches your project structure
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        playwrite: ['"Playwrite PL"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
