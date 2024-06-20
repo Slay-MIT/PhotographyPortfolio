5ad6fa96de947ba5f35f903b5b4ff03d89c06608
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-06-20 11:24:24 +0530

    commit

--- my-first-site/src/sections/SectionTwo.js
+++ my-first-site/src/sections/SectionTwo.js
@@ -3,7 +3,7 @@ import Cards from '../components/Cards';
 
 function SectionTwo() {
   return (
-    <section className="h-screen bg-gray-100 flex justify-center items-center snap-center scroll-smooth">
+    <section className="h-screen flex justify-center items-center snap-center scroll-smooth">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-8">
         {/* Use the Cards component */}
         <Cards />
