8396029fee1b78a1965dd5574d1087dd4ca6d08b
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-06-24 00:08:33 +0530

    overflow-scroll to auto, bottom bar gone

--- my-first-site/src/App.js
+++ my-first-site/src/App.js
@@ -6,7 +6,7 @@ import SectionThree from './sections/SectionThree';
 function App() {
   return (
     <div className="App">
-      <div className=' bg-slate-800 snap-y snap-mandatory h-screen overflow-scroll snap-center scroll-smooth'>
+      <div className=' bg-slate-800 snap-y snap-mandatory h-screen overflow-auto snap-center scroll-smooth'>
       <Header></Header>
       <SectionTwo></SectionTwo>
       <SectionThree></SectionThree>
