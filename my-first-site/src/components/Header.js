009f618e5d30f469e31f2ae5f13c35d70545422e
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-06-28 01:42:39 +0530

    changes to default dark and light mode

--- my-first-site/src/components/Header.js
+++ my-first-site/src/components/Header.js
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 function Header() {
   const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem('darkMode') === 'false';
+    return localStorage.getItem("darkMode");
   });
 
   useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
+    if (darkMode !== 'dark') {
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
     }
-    localStorage.setItem('darkMode', darkMode);
+    localStorage.setItem("darkMode", darkMode);
+    console.log(darkMode);
   }, [darkMode]);
 
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
+    setDarkMode(darkMode === 'dark' ? 'light' : 'dark');
   };
 
   return (
-    <div className="flex justify-center items-center h-screen snap-center scroll-smooth ">
+    <div className="flex justify-center items-center h-screen snap-center scroll-smooth " >
+
       <motion.h1
-        className='font-bold underline font-playwrite text-6xl dark:text-blue-900 text-yellow-400 hover:text-red-600 dark:hover:text-green-500 transition-all duration-500 hover:cursor-pointer'
+        className="font-bold underline font-playwrite text-6xl dark:text-blue-900 text-yellow-400 hover:text-red-600 dark:hover:text-green-500 transition-all duration-500 hover:cursor-pointer"
         whileHover={{ scale: 1.5 }} // Slightly increase size on hover
-        transition={{ type: 'tween', stiffness: 300, damping: 50 }} // Define the type of transition
+        transition={{ type: "tween", stiffness: 300, damping: 50 }} // Define the type of transition
         onClick={toggleDarkMode} // Toggle dark mode on click
       >
         Shreyash Shubh
