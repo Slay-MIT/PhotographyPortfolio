8bc430055c3a5253e7ffc3abf0021795f0698692
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-06-29 15:45:00 +0530

    changed all files to webp and added to git lfs

--- my-first-site/src/components/imageData.js
+++ my-first-site/src/components/imageData.js
@@ -1,27 +1,27 @@
 // imageData.js
 const imageData = {
     Abstract: [
-      "/Grid Images/Abstract/DSCF0287-2.jpg",
-      "/Grid Images/Abstract/DSCF0311.jpg",
-      "/Grid Images/Abstract/DSCF7320-9.jpg",
+      "/Grid Images/Abstract/DSCF0287-2.webp",
+      "/Grid Images/Abstract/DSCF0311.webp",
+      "/Grid Images/Abstract/DSCF7320-9.webp",
     ],
     Architecture: [
-      "/Grid Images/Architecture/DSCF0567.jpg",
-      "/Grid Images/Architecture/DSCF0763-9.jpg",
-      "/Grid Images/Architecture/DSCF1466.jpg",
-      "/Grid Images/Architecture/DSCF8588.jpg",
+      "/Grid Images/Architecture/DSCF0567.webp",
+      "/Grid Images/Architecture/DSCF0763-9.webp",
+      "/Grid Images/Architecture/DSCF1466.webp",
+      "/Grid Images/Architecture/DSCF8588.webp",
     ],
     Concert: [
-      "/Grid Images/Concert/DSCF3487.jpg",
-      "/Grid Images/Concert/DSCF3984.jpg",
-      "/Grid Images/Concert/DSCF4288.jpg",
-      "/Grid Images/Concert/IMG_3405.jpg",
-      "/Grid Images/Concert/IMG_3981.jpg",
+      "/Grid Images/Concert/DSCF3487.webp",
+      "/Grid Images/Concert/DSCF3984.webp",
+      "/Grid Images/Concert/DSCF4288.webp",
+      "/Grid Images/Concert/IMG_3405.webp",
+      "/Grid Images/Concert/IMG_3981.webp",
     ],
     Landscape: [
-      "/Grid Images/Landscape/_A190012.jpg",
-      "/Grid Images/Landscape/DSCF1580.jpg",
-      "/Grid Images/Landscape/DSCF4382.jpg",
+      "/Grid Images/Landscape/_A190012.webp",
+      "/Grid Images/Landscape/DSCF1580.webp",
+      "/Grid Images/Landscape/DSCF4382.webp",
     ],
   };
   
--- my-first-site/src/sections/SectionThree.js
+++ my-first-site/src/sections/SectionThree.js
@@ -4,7 +4,7 @@ import imageData from "../components/imageData";
 function SectionThree() {
   return (
     <section className="overflow-y-auto flex flex-col scroll-smooth">
-      <nav className="flex items-center p-4 bg-gray-800">
+      <nav className="flex items-center p-4 bg-gray-800 padding">
         <ul className="flex space-x-4">
           <li>
             <button className="text-white">Abstract</button>
@@ -21,20 +21,135 @@ function SectionThree() {
         </ul>
       </nav>
       <div className="flex justify-center items-center flex-grow overflow-auto">
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 overflow-auto">
-          {Object.keys(imageData).map((category) => (
-            <div key={category} className="grid gap-4">
-              {imageData[category].map((src, index) => (
-                <div key={index}>
-                  <img
-                    className="h-auto max-w-full rounded-lg object-cover object-center"
-                    src={src}
-                    alt={`${category} gallery-photo ${index + 1}`}
-                  />
-                </div>
-              ))}
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+          <div className="grid gap-4">
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Abstract/DSCF0287-2.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
             </div>
-          ))}
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Abstract/DSCF0311.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Abstract/DSCF7320-9.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+          </div>
+          <div className="grid gap-4">
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Architecture/DSCF0567.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Architecture/DSCF0763-9.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Architecture/DSCF1466.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Architecture/DSCF8588.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+          </div>
+          <div className="grid gap-4">
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Concert/DSCF3487.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Concert/DSCF3984.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Concert/DSCF4288.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Concert/IMG_3405.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Concert/IMG_3981.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+          </div>
+          <div className="grid gap-4">
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Landscape/_A190012.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Landscape/DSCF1580.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full rounded-lg object-cover object-center"
+                src="/Grid Images/Landscape/DSCF4382.webp"
+                alt="gallery-photo"
+                loading="lazy" // Add lazy loading attribute
+              />
+            </div>
+          </div>
         </div>
       </div>
     </section>
