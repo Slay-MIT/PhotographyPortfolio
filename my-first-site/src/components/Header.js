100e47cb56d64fedb8218d833b5964fd8984e8a9
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-07-02 20:44:01 +0530

    header colour changes

--- my-first-site/src/components/Header.js
+++ my-first-site/src/components/Header.js
@@ -22,7 +22,7 @@ function Header() {
   return (
     <div className="flex justify-center items-center h-screen scroll-smooth relative bottom-9">
       <motion.h1
-        className="font-bold font-playwrite text-6xl dark:text-emerald-300 text-indigo-700 hover:text-indigo-900 dark:hover:text-emerald-500 transition-all duration-500 hover:cursor-pointer text-shadow-lg text-pretty"
+        className="font-bold font-playwrite text-6xl dark:text-emerald-300 text-indigo-700 hover:text-sky-500 dark:hover:text-emerald-500 transition-all duration-500 hover:cursor-pointer text-shadow-lg text-pretty"
         whileHover={{ scale: 1.5 }} // Slightly increase size on hover
         transition={{ type: "tween", stiffness: 300, damping: 50 }} // Define the type of transition
         onClick={toggleDarkMode} // Toggle dark mode on click
