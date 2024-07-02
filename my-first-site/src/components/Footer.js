87b2e1ba3bc29567908260d39807a0f6e90edf9f
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-07-02 20:11:52 +0530

    footer changes

--- my-first-site/src/components/Footer.js
+++ my-first-site/src/components/Footer.js
@@ -3,35 +3,26 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 py-4 w-full">
-      <div className="  flex justify-between items-center px-4">
+      <div className="flex justify-between items-center px-4">
         <a
           href="https://www.instagram.com/shreyashshubh"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center space-x-2"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"
+            alt="Instagram Logo"
             className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M7 7h.01M4 6h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2zm3 0a4 4 0 100 8 4 4 0 000-8zm13 6a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
-          <span>Instagram</span>
+          />
+          <span className='dark:text-slate-300 text-slate-700'>Instagram</span>
         </a>
-        <button
-          onClick={() => alert('Contact me at: your-email@example.com')}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        <a
+          href="mailto:shreyash148@gmail.com"
+          className="text-blue-500 hover:underline dark:text-blue-300"
         >
           Contact Me
-        </button>
+        </a>
       </div>
     </footer>
   );
